a159b4d0944ffc1de344269b766a4cf2681d60ea
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-10-18 23:02:16 -0300

    refactor: separando as configuracoes de docs

--- src/Users.API/Middlewares/Configurations.cs
+++ src/Users.API/Middlewares/Configurations.cs
@@ -41,65 +41,6 @@ namespace Users.API.Middlewares
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty));
         }
 
-        public static void SwaggerDocumentation(this WebApplicationBuilder builder)
-        {
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo()
-                {
-                    Title = "Lerning Code Enterprise API",
-                    Contact = new OpenApiContact() { Name = "Guilherme Nascimento", Email = "guirafaelrn@gmail.com" },
-                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT") }
-                });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Insira o token JWT desta forma: Bearer {seu token}",
-                    Name = "Authorization",
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-                c.AddSecurityDefinition("AcessSecretKey", new OpenApiSecurityScheme
-                {
-                    Description = "Chave de acesso necessária para acessar o sistema",
-                    Name = "AcessSecretKey",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "AcessSecretKey"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-            });
-        }
-
         public static void SecurityMiddleware(this WebApplicationBuilder builder)
         {
             var jwtDataSection = builder.Configuration.GetSection("Jwt");
---
+++ src/Users.API/Middlewares/DocsConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Users.API.Middlewares
+{
+    public static class DocsConfiguration
+    {
+        public static void SwaggerDocumentation(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Lerning Code Enterprise API",
+                    Contact = new OpenApiContact() { Name = "Guilherme Nascimento", Email = "guirafaelrn@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT") }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta forma: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+                c.AddSecurityDefinition("AcessSecretKey", new OpenApiSecurityScheme
+                {
+                    Description = "Chave de acesso necessária para acessar o sistema",
+                    Name = "AcessSecretKey",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "AcessSecretKey"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+    }
+}
