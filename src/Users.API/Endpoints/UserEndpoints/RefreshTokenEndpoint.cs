0da509cdb74055fccdad86157c2f979dd296da54
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-10-25 18:32:09 -0300

    feat: adicionado refresh token

---
+++ src/Users.API/Endpoints/UserEndpoints/RefreshTokenEndpoint.cs
@@ -0,0 +1,31 @@
+﻿
+using MediatR;
+using System.Security.Claims;
+using Users.Application.Command.LoginUser;
+using Users.Application.Command.RefreshTokenUser;
+using Users.Application.DTOs;
+using Users.Application.Responses;
+using Users.Domain.Repositories;
+
+namespace Users.API.Endpoints.UserEndpoints
+{
+    public class RefreshTokenEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app) =>
+            app.MapPost("/refresh", HandleAsync).Produces<Response<GetUserTokensDTO?>>();
+
+        public static async Task<IResult> HandleAsync(IMediator mediator,
+                                                      ClaimsPrincipal user,
+                                                      RefreshTokenUserCommand command)
+        {
+            var userName = user.Identity!.Name;
+            var userIdClaim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            var result = await mediator.Send(new RefreshTokenUserCommand(command.Token, command.RefreshToken,
+                                                                         userName!, Guid.Parse(userIdClaim!.Value)));
+
+            return result.IsSuccess
+                ? TypedResults.Ok(result)
+                : TypedResults.BadRequest(result);
+        }
+    }
+}
